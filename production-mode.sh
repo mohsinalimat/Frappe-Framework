d07fc5aaa3ebbc0472fa29fd9107e9c4a61c2805
Ranjith1022 <103019099+Ranjith1022@users.noreply.github.com>
2022-07-21 11:57:43 +0530

    Update production-mode.sh

--- production-mode.sh
+++ production-mode.sh
@@ -1,4 +1,11 @@
 cd ~
+#Go to file path
 cd frappe-bench
+#Find username
 me=$(whoami)
+#Froduction setup
 sudo bench setup production ${me} --yes
+#Update the the production mode
+sudo apt update
+#This file to run every 10 minutes. but, it requires only one time so this file removed
+sudo rm -rf /home/production-mode.sh
