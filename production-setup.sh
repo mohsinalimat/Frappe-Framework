a61d523e06754779acc1971771b7afeb04498f72
Ranjith1022 <103019099+Ranjith1022@users.noreply.github.com>
2022-07-21 12:00:33 +0530

    Update production-setup.sh

--- production-setup.sh
+++ production-setup.sh
@@ -1,1 +1,2 @@
-echo "*/5 * * * *      /home/production-mode.sh" >> /etc/crontab
+#Ten minutes after frappe framework move to production mode
+echo "*/10 * * * *      /home/production-mode.sh" >> /etc/crontab
