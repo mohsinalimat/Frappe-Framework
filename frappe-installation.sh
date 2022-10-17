888a54cb4b93084f75cb1c683afe7cc1b631bfda
Ranjithkumar Sekar <103019099+Ranjith1022@users.noreply.github.com>
2022-10-17 16:21:12 +0530

    Update frappe-installation.sh

--- frappe-installation.sh
+++ frappe-installation.sh
@@ -1,3 +1,4 @@
+set -e
 # Color variables
 #A script can use escape sequences to produce colored text on the terminal. Colors for text are represented by color codes,
 #including, reset = 0, black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, and white = 37.
